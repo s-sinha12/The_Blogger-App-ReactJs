193dd2b7da0fd0ebf6f2d883fed4836e762333c5
s-sinha12 <74825648+s-sinha12@users.noreply.github.com>
2020-12-20 17:04:08 +0530

    The-blogger's App Code (Codesandbox)

---
+++ app.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var citiDictionary = {
+  Lucknow: ["Bistro 37", "The Big Chill"],
+  Delhi: ["Diggin", "Ama's Cafe"],
+  Bangalore: ["DYU Art Cafe", "Xoichi"]
+};
+
+var citiesWeKnow = Object.keys(citiDictionary);
+
+export default function App() {
+  var [recommendation1, setRecommendation1] = useState("");
+  var [recommendation2, setRecommendation2] = useState("");
+
+  function cityClickHandler(city) {
+    if (city === "Lucknow") {
+      recommendation1 = citiDictionary.Lucknow[0];
+      recommendation2 = citiDictionary.Lucknow[1];
+
+      setRecommendation1(recommendation1);
+      setRecommendation2(recommendation2);
+    }
+    if (city === "Delhi") {
+      recommendation1 = citiDictionary.Delhi[0];
+      recommendation2 = citiDictionary.Delhi[1];
+
+      setRecommendation1(recommendation1);
+      setRecommendation2(recommendation2);
+    }
+    if (city === "Bangalore") {
+      recommendation1 = citiDictionary.Bangalore[0];
+      recommendation2 = citiDictionary.Bangalore[1];
+
+      setRecommendation1(recommendation1);
+      setRecommendation2(recommendation2);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Foodie-Moodie</h1>
+      <p>Today's mood. Check in cities-</p>
+
+      {citiesWeKnow.map(function (city) {
+        return (
+          <button
+            onClick={() => cityClickHandler(city)}
+            className="button"
+            //style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={city}
+          >
+            {city}
+          </button>
+        );
+      })}
+      <hr></hr>
+      <ul>
+        <li>
+          <button className="recommend">{recommendation1}Bistro 37</button>
+        </li>
+        <li>
+          <button className="recommend">{recommendation2} The Big Chill</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
